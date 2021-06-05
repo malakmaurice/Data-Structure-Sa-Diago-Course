8173f0e707eea859854599577b077e60da6555de
malakmaurice <43122053+malakmaurice@users.noreply.github.com>
2021-06-05 22:53:00 +0200

    Update main.cpp

--- week4 Assignments/find pattern in text/main.cpp
+++ week4 Assignments/find pattern in text/main.cpp
@@ -6,44 +6,48 @@ using namespace std;
 long long THash[500000];
 vector<int>result;
 long long prim=1000081;
-long long PolyHash(string p,int x)
+int x =rand()% prim+1;
+long long PolyHash(string P)
 {
     long long h=0;
-    for(int i=p.length()-1; i>=0; i--)
-        h=((h * x+p[i])%prim +prim)%prim;
+    for(int i=P.length()-1; i>=0; i--)
+        h=((h * x+P[i])%prim+prim)%prim;
     return h;
 }
-void PrecomputeHashes(string T,int len,int x){
-    string s=T.substr(T.length()-len,len);
-    THash[T.length()-len]=PolyHash(s,x);
+void PrecomputeHashes(string T,int len)
+{
+    string s=T.substr(T.length()-len,T.length()-1);
+    THash[T.length()-len]=PolyHash(s);
     int y=1;
-    for(int i=1;i<=len;i++)
-        y=((y*x)%prim+prim)%prim;
-    for(int i= T.length()-len-1;i>=0;i--)
-        THash[i]=(x*THash[i+1]+T[i]-y*T[i+len])%prim;
+    for(int i=1; i<=len; i++)
+        y=(y*x)%prim;
+    for(int i= T.length()-len-1; i>=0; i--)
+        THash[i]=((x*THash[i+1])%prim+(T[i])%prim-(y*T[i+len])%prim+prim)%prim;
 }
 void RabinKarp(string T,string P)
 {
-    int x =rand()% prim+1;
-    long long PHash=PolyHash(P,x);
-    PrecomputeHashes(T,P.length(),x);
-    for(int i=0; i<T.length()-P.length(); i++)
+
+    long long PHash=PolyHash(P);
+    //cout<<"PHash = "<<PHash<<"\n";
+    PrecomputeHashes(T,P.length());
+    for(int i=0; i<=T.length()-P.length(); i++)
     {
+      //  cout<<"THash"<< i<<" = "<<THash[i]<<"\n";
         if(PHash!=THash[i])
             continue;
         if(P.compare(T.substr(i,P.length()))==0)
             result.push_back(i);
-        }
+    }
 }
 int main()
-{string T,P;
+{
+    string T,P;
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     cin>>P>>T;
 
-RabinKarp(T,P);
-for(int i=0;i<result.size();i++)
-    cout<<result[i]<<endl;
-
+    RabinKarp(T,P);
+    for(int i=0; i<result.size(); i++)
+cout<<result[i]<<" ";
     return 0;
 }
